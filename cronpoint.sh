850bde4c9aa3eb45dfc971c54c9e0fcfb72c1c0a
Morgan Zero <57449785+morganzero@users.noreply.github.com>
2023-07-23 23:00:38 +0200

    Update cronpoint.sh

--- cronpoint.sh
+++ cronpoint.sh
@@ -20,45 +20,47 @@ if [ -n "$ENV_FILE" ] && [ -f "$ENV_FILE" ]; then
   source "$ENV_FILE"
 fi
 
-container_ids=$(docker ps -q -f label=mycron.enabled=true)
+while true; do
+  container_ids=$(docker ps -q -f label=mycron.enabled=true)
 
-for id in $container_ids
-do
-  echo -e "${YELLOW}Processing container ID: ${id}${NC}"
-  labels=$(docker inspect --format '{{json .Config.Labels}}' $id)
+  for id in $container_ids
+  do
+    echo -e "${YELLOW}Processing container ID: ${id}${NC}"
+    labels=$(docker inspect --format '{{json .Config.Labels}}' $id)
 
-  mycron_labels=$(echo $labels | jq -r 'to_entries[] | select(.key | startswith("mycron")) | .key + "=" + .value' )
-  echo -e "${GREEN}mycron labels:${NC}"
-  echo "$mycron_labels"
+    mycron_labels=$(echo $labels | jq -r 'to_entries[] | select(.key | startswith("mycron")) | .key + "=" + .value' )
+    echo -e "${GREEN}mycron labels:${NC}"
+    echo "$mycron_labels"
   
-  declare -A job_data
+    declare -A job_data
 
-  while IFS= read -r label; do
-    key=$(echo $label | cut -d= -f1)
-    value=$(echo $label | cut -d= -f2)
-    job_id=$(echo $key | cut -d. -f2)
-    attribute=$(echo $key | cut -d. -f3)
-    job_data[$job_id.$attribute]=$value
-  done <<< "$mycron_labels"
+    while IFS= read -r label; do
+      key=$(echo $label | cut -d= -f1)
+      value=$(echo $label | cut -d= -f2)
+      job_id=$(echo $key | cut -d. -f2)
+      attribute=$(echo $key | cut -d. -f3)
+      job_data[$job_id.$attribute]=$value
+    done <<< "$mycron_labels"
 
-  for job_id_attribute in "${!job_data[@]}"; do
-    job_id=$(echo $job_id_attribute | cut -d. -f1)
-    if [[ ${job_data[$job_id.schedule]} && ${job_data[$job_id.command]} ]]; then
-      schedule=${job_data[$job_id.schedule]}
-      command=${job_data[$job_id.command]}
+    for job_id_attribute in "${!job_data[@]}"; do
+      job_id=$(echo $job_id_attribute | cut -d. -f1)
+      if [[ ${job_data[$job_id.schedule]} && ${job_data[$job_id.command]} ]]; then
+        schedule=${job_data[$job_id.schedule]}
+        command=${job_data[$job_id.command]}
       
-      echo -e "${GREEN}Job details:${NC}"
-      echo "Job ID: $job_id"
-      echo "Container ID: $id"
-      echo "Schedule: $schedule"
-      echo "Command: $command"
-      echo "Monitoring job $job_id for container $id. It will be executed as per schedule: $schedule"
+        echo -e "${GREEN}Job details:${NC}"
+        echo "Job ID: $job_id"
+        echo "Container ID: $id"
+        echo "Schedule: $schedule"
+        echo "Command: $command"
+        echo "Monitoring job $job_id for container $id. It will be executed as per schedule: $schedule"
       
-      # Execute the command according to the schedule
-      # Add your logic here
-    fi
+        # Execute the command according to the schedule
+        # Add your logic here
+      fi
+    done
+    unset job_data
   done
-  unset job_data
-done
 
-tail -f /dev/null
+  sleep 60
+done
